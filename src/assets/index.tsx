28015d30562a283a3006cd22e9c775b14dbd7a10
gracebir <grace.devolop@gmail.com>
2023-04-28 20:19:46 +0200

    organize icons

---
+++ src/assets/index.tsx
@@ -0,0 +1,54 @@
+import gitstart from './gitstart.png'
+import distributed from './distributed.jpeg'
+import company from './companing.png'
+
+import html from '../assets/icons/vscode-icons_file-type-html.svg'
+import css from '../assets/icons/vscode-icons_file-type-css.svg'
+import javascript from '../assets/icons/vscode-icons_file-type-js-official.svg'
+import react from '../assets/icons/logos_react.svg'
+import electron from '../assets/icons/Vector.svg'
+import gitlogo from '../assets/icons/logos_git-icon.svg'
+import cibgreen from '../assets/icons/cib_greensock.svg'
+import bootstrap from '../assets/icons/logos_bootstrap.svg'
+import tailwindcss from '../assets/icons/vscode-icons_file-type-tailwind.svg'
+import sass from '../assets/icons/logos_sass.svg'
+import vscode from '../assets/icons/vscode-icons_file-type-vscode.svg'
+import github from '../assets/icons/akar-icons_github-fill.svg'
+
+import backend from "./backend.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import project1 from '../assets/Rectangle 16.svg'
+import project2 from '../assets/Rectangle 18.svg'
+import project3 from '../assets/Rectangle 8.svg'
+import project4 from '../assets/Rectangle 24.svg'
+import project5 from '../assets/Rectangle 23.svg'
+import project6 from '../assets/Rectangle 22.svg'
+
+export { 
+    gitstart, 
+    distributed, 
+    company, 
+    html,
+    css,
+    javascript,
+    react,
+    electron,
+    gitlogo,
+    cibgreen,
+    bootstrap,
+    tailwindcss,
+    sass,
+    vscode,
+    github,
+    project1,
+    project2,
+    project3,
+    project4,
+    project5,
+    project6,
+    web,
+    mobile,
+    backend
+}
