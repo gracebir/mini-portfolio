0f0e6930bddd0a4f8ccc43b4a68f02017afb6aef
gracebir <grace.devolop@gmail.com>
2023-04-28 23:37:21 +0200

    adding classes

--- src/utils/classNames.tsx
+++ src/utils/classNames.tsx
@@ -2,5 +2,5 @@ export const styles = {
     Title: "text-5xl font-bold text-center",
     SubTile: "text-4xl text-center",
     FlexCol: "flex flex-col gap-12",
-    sections: "mb-36 flex flex-col gap-32 py-6"
+    sections: "mb-36 flex flex-col gap-32 py-6",
 }
