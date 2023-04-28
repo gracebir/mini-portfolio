832b2d0cbbd5a80f401be53c944f78ea928771d9
gracebir <grace.devolop@gmail.com>
2023-04-28 23:36:49 +0200

    contact components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,19 @@
+import { AiFillTwitterCircle } from 'react-icons/ai'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
 function Contact() {
   return (
-    <div>
-      contact
+    <div className="flex flex-col gap-4 lg:flex-row lg:justify-between items-center">
+      <a href="#home" className="bg-gradient-to-r from-[#13B0F5] text-transparent via-[#E70FAA] to-[#E70FAA] bg-clip-text text-2xl font-extrabold">{"{ Grac. }"}</a>
+      <div className="flex flex-col lg:flex-row gap-4 lg:gap-10">
+        <span className="text-[18px] font-mediu">+250 780 869 394</span>
+        <span className="text-[18px] font-medium">birindwan@gmail.com</span>
+        <div className="flex gap-4 items-center">
+          <a href="#"><BsGithub size={23} /></a>
+          <a href="#"><AiFillTwitterCircle size={25} /></a>
+          <a href="#"><BsLinkedin size={25} /></a>
+        </div>
+      </div>
     </div>
   )
 }
