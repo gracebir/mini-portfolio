b67e52dcfa72a25718e9687cc973c0c9992c9e76
gracebir <grace.devolop@gmail.com>
2023-08-06 20:03:58 +0200

    remove the old project array

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { styles } from "../utils/classNames"
-import { projectData } from "../utils/data"
+import { projects } from "../utils/data"
 import Card from "./Card"
 
 function Projects() {
@@ -10,14 +10,14 @@ function Projects() {
         <p className={`${styles.SubTile}`}>Things I’ve built so far</p>
       </div>
       <div className="grid grid-cols-1 place-items-center md:grid-cols-2 lg:grid-cols-3 gap-16">
-        {projectData.map((project, i)=> (
-          <Card 
-          key={i} 
-          title="Project Tile goes here" 
-          description="This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content" 
-          image={project.image}/>
+        {projects.map((project, _) => (
+          <Card
+            key={project.id}
+            title={project.title}
+            techStacks={project.techStacks}
+            description={project.description}
+            image={project.image} />
         ))}
-        
       </div>
     </section>
   )
