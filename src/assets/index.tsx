06ac2fb9e1bfed50e971a98e8228e2700506ed32
gracebir <grace.devolop@gmail.com>
2023-10-29 18:33:36 +0200

    replace age calculator with memory game

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,45 +1,46 @@
-import gitstart from './gitstart.png'
-import distributed from './distributed.jpeg'
-import company from './companing.png'
+/** @format */
 
-import html from '../assets/icons/vscode-icons_file-type-html.svg'
-import css from '../assets/icons/vscode-icons_file-type-css.svg'
-import javascript from '../assets/icons/vscode-icons_file-type-js-official.svg'
-import react from '../assets/icons/logos_react.svg'
-import electron from '../assets/icons/Vector.svg'
-import gitlogo from '../assets/icons/logos_git-icon.svg'
-import cibgreen from '../assets/icons/cib_greensock.svg'
-import bootstrap from '../assets/icons/logos_bootstrap.svg'
-import tailwindcss from '../assets/icons/vscode-icons_file-type-tailwind.svg'
-import sass from '../assets/icons/logos_sass.svg'
-import vscode from '../assets/icons/vscode-icons_file-type-vscode.svg'
-import github from '../assets/icons/akar-icons_github-fill.svg'
-import python from '../assets/logopython.png'
-import express from '../assets/expressjs-icon.svg'
+import gitstart from "./gitstart.png";
+import distributed from "./distributed.jpeg";
+import company from "./companing.png";
+
+import html from "../assets/icons/vscode-icons_file-type-html.svg";
+import css from "../assets/icons/vscode-icons_file-type-css.svg";
+import javascript from "../assets/icons/vscode-icons_file-type-js-official.svg";
+import react from "../assets/icons/logos_react.svg";
+import electron from "../assets/icons/Vector.svg";
+import gitlogo from "../assets/icons/logos_git-icon.svg";
+import cibgreen from "../assets/icons/cib_greensock.svg";
+import bootstrap from "../assets/icons/logos_bootstrap.svg";
+import tailwindcss from "../assets/icons/vscode-icons_file-type-tailwind.svg";
+import sass from "../assets/icons/logos_sass.svg";
+import vscode from "../assets/icons/vscode-icons_file-type-vscode.svg";
+import github from "../assets/icons/akar-icons_github-fill.svg";
+import python from "../assets/logopython.png";
+import express from "../assets/expressjs-icon.svg";
 
 import backend from "./backend.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 
-import project1 from '../assets/Rectangle 16.svg'
-import project2 from '../assets/Rectangle 18.svg'
-import project3 from '../assets/Rectangle 8.svg'
-import project4 from '../assets/Rectangle 24.svg'
-import project5 from '../assets/Rectangle 23.svg'
-import project6 from '../assets/Rectangle 22.svg'
+import project1 from "../assets/Rectangle 16.svg";
+import project2 from "../assets/Rectangle 18.svg";
+import project3 from "../assets/Rectangle 8.svg";
+import project4 from "../assets/Rectangle 24.svg";
+import project5 from "../assets/Rectangle 23.svg";
+import project6 from "../assets/Rectangle 22.svg";
 
-import multiForm from '../assets/projects/multi-step.jpg'
-import country from '../assets/projects/countries.jpg'
-import amazon from '../assets/projects/amazon.png'
-import buymore from '../assets/projects/buymore.png'
-import landing from '../assets/projects/landing-page.jpg'
-import ageCalculator from '../assets/projects/calculator.jpg'
+import multiForm from "../assets/projects/multi-step.jpg";
+import country from "../assets/projects/countries.jpg";
+import amazon from "../assets/projects/amazon.png";
+import buymore from "../assets/projects/buymore.png";
+import landing from "../assets/projects/landing-page.jpg";
+import memoryGame from "../assets/projects/memory-game.png";
 
-
-export { 
-    gitstart, 
-    distributed, 
-    company, 
+export {
+    gitstart,
+    distributed,
+    company,
     html,
     css,
     javascript,
@@ -60,14 +61,13 @@ export {
     project4,
     project5,
     project6,
-
     landing,
     multiForm,
     country,
     amazon,
     buymore,
-    ageCalculator,
+    memoryGame,
     web,
     mobile,
-    backend
-}
+    backend,
+};
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,30 @@
-import profile from "../assets/da.avif"
+/** @format */
+
+import profile from "../assets/profile.jpg";
 
 function Hero() {
     return (
-        <section id="home" className="flex flex-col-reverse gap-20 md:mt-0 lg:mt-0 px-4 lg:px-0 lg:py-0 items-center lg:flex-row lg:justify-between lg:items-center h-[100vh] mb-32 lg:h-[87vh]">
-            <h1 className="lg:text-6xl text-4xl font-bold lg:leading-[70px] leading-[50px]">
-                Hi 👋,<br /> My name is <br />
-                <span className="bg-gradient-to-r from-[#13B0F5] text-transparent via-[#13B0F5] to-[#E70FAA] bg-clip-text">Grace Birindwa</span> <br /> I build things for web
+        <section
+            id='home'
+            className='flex flex-col-reverse gap-20 md:mt-0 lg:mt-0 px-4 lg:px-0 lg:py-0 items-center lg:flex-row lg:justify-between lg:items-center h-[100vh] mb-32 lg:h-[87vh]'
+        >
+            <h1 className='lg:text-6xl text-4xl font-bold lg:leading-[70px] leading-[50px]'>
+                Hi 👋,
+                <br /> My name is <br />
+                <span className='bg-gradient-to-r from-[#13B0F5] text-transparent via-[#13B0F5] to-[#E70FAA] bg-clip-text'>
+                    Grace Birindwa
+                </span>{" "}
+                <br /> I build things for web
             </h1>
-            <div className="border-8 bg-gradient-to-b w-[330px] lg:w-[350px] p-2 h-[330px] lg:h-[350px] rounded-full from-[#E70FAA] border-transparent via-[#13B0F5] to-[#E70FAA] bg-clip-border">
-                <img className="w-full h-full rounded-full border objet-fit" src={profile} alt="" />
+            <div className='border-8 bg-gradient-to-b w-[330px] lg:w-[350px] p-2 h-[330px] lg:h-[350px] rounded-full from-[#E70FAA] border-transparent via-[#13B0F5] to-[#E70FAA] bg-clip-border'>
+                <img
+                    className='w-full h-full rounded-full border object-cover'
+                    src={profile}
+                    alt=''
+                />
             </div>
         </section>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
